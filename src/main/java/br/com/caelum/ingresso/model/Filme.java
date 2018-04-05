0a9d22ccc4dad0854687dc769be79bc0a2a4d2a8
Vinícius Dias <vintheman@gmail.com>
2018-04-05 11:39:16 -0300

    implementação do campo 'preço' em sala, filme e sessão. suas regras e testes, também

--- src/main/java/br/com/caelum/ingresso/model/Filme.java
+++ src/main/java/br/com/caelum/ingresso/model/Filme.java
@@ -3,6 +3,9 @@ package br.com.caelum.ingresso.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.Duration;
 
 /**
@@ -14,10 +17,11 @@ public class Filme {
     @Id
     @GeneratedValue
     private Integer id;
+    
     private String nome;
     private Duration duracao;
     private String genero;
-    
+    private BigDecimal preco;
     
     
     /**
@@ -27,10 +31,23 @@ public class Filme {
 
     }
     
-    public Filme(String nome, Duration duracao, String genero) {
+    public Filme(String nome, Duration duracao, String genero, BigDecimal preco) {
 		this.nome = nome;
 		this.duracao = duracao;
 		this.genero = genero;
+		this.preco = preco;
+	}
+
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public void setDuracao(Duration duracao) {
+		this.duracao = duracao;
 	}
 
 	public Integer getId() {
